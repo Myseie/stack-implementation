f4766872f245358974c031d4d3a84fbb6369ea82
Myseie <123580372+Myseie@users.noreply.github.com>
2024-09-22 14:13:35 +0200

    Add files via upload

---
+++ stack implementation/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+class Program
+{
+    static void Main()
+    {
+        Stack<int> stack = new Stack<int>();
+
+        stack.Push(10);
+        stack.Push(20);
+        stack.Push(30);
+
+        Console.WriteLine($"Översta elementet i stacken är : {stack.Peek()}");
+
+        stack.Pop();
+        stack.Pop();
+
+        Console.WriteLine($"Översta elementet i stacken är : {stack.Peek()}");
+
+    }
+}
---
+++ stack implementation/Stack.cs
@@ -0,0 +1,48 @@
+﻿using Elmah.ContentSyndication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stack_implementation
+{
+    class Stack<T>
+    {
+        private List<T> elements = new List<T>();
+
+        public void Push(T item)
+        {
+            elements.Add(item);
+            Console.WriteLine($"{item} har lagts till stacken");
+        }
+
+        public T Pop()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidCastException("Stacken är tom");
+            }
+
+            T value = elements[elements.Count - 1];
+            elements.RemoveAt(elements.Count - 1);
+            Console.WriteLine($"{value} har tagits bort från stacken");
+            return value;
+        }
+
+        public T Peek()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidCastException("Stacken är tom");
+            }
+
+            return elements[elements.Count - 1];
+        }
+
+        public bool IsEmpty()
+        {
+            return elements.Count == 0;
+        }
+    }
+}
